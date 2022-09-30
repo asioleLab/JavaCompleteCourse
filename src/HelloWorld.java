1f19ebf1bb648f7f8e541904abda5d1f1f128d3d
Escarsel <eloisa.scarsella@everis.com>
2022-09-30 17:00:18 +0200

    Print hello world

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,9 @@
 public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello, this is my first java program");
+
+        int firstNumber=5;
+        System.out.println("The value of the variable firstNumber is "+firstNumber);
+    }
 }
