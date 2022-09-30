999e9f8c3bebcf40b9cc3c9ce60efe827096ea30
Escarsel <eloisa.scarsella@everis.com>
2022-09-30 17:19:28 +0200

    byte, short minimum and maximum value

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,5 +5,18 @@ public class HelloWorld {
 
         int firstNumber=5;
         System.out.println("The value of the variable firstNumber is "+firstNumber);
+
+        int minIntValue=Integer.MIN_VALUE;
+        int maxIntValue=Integer.MAX_VALUE;
+        System.out.println("Integer minimum value = "+ minIntValue + " and maximum value = " + maxIntValue);
+
+        byte minByteValue=Byte.MIN_VALUE;
+        byte maxByteValue=Byte.MAX_VALUE;
+        System.out.println("Byte minimum value = "+ minByteValue + " and maximum value = " + maxByteValue);
+
+        short minShortValue=Short.MIN_VALUE;
+        short maxShortValue=Short.MAX_VALUE;
+        System.out.println("Short minimum value = "+ minShortValue + " and maximum value = " + maxShortValue);
+
     }
 }
