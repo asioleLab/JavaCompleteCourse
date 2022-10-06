95b2a727f21457c32916b465e33e88e14e168527
Escarsel <eloisa.scarsella@everis.com>
2022-10-06 17:11:45 +0200

    Inheritance excercises

---
+++ src/InheritanceExercise/ex1/Cuboid.java
@@ -0,0 +1,24 @@
+package InheritanceExercise.ex1;
+
+public class Cuboid extends Rectangle{
+
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        if(height<0){
+            this.height=0;
+        }else{
+            this.height = height;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume(){
+        return super.getArea() * this.height;
+    }
+
+}
