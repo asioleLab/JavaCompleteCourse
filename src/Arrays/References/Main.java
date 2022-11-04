8402f89e298a1519572076fec7ff0c76d0225976
escarsel <eloisa.scarsella@nttdata.com>
2022-11-04 17:57:30 +0100

    Copy by referencies vs assign

--- src/Arrays/References/Main.java
+++ src/Arrays/References/Main.java
@@ -12,12 +12,24 @@ public class Main {
 
         System.out.println("myArray " + Arrays.toString(myArray));
         System.out.println("anotherArray " + Arrays.toString(anotherArray));
-        System.out.println("anotherArray " + Arrays.toString(thirdArray));
+        System.out.println("thirdArray " + Arrays.toString(thirdArray));
 
         anotherArray[0]=1;
 
-        System.out.println("myArray " + Arrays.toString(myArray));
-        System.out.println("anotherArray " + Arrays.toString(anotherArray));
-        System.out.println("anotherArray " + Arrays.toString(thirdArray));
+        System.out.println("after change myArray " + Arrays.toString(myArray));
+        System.out.println("after change anotherArray " + Arrays.toString(anotherArray));
+        System.out.println("after change thirdArray " + Arrays.toString(thirdArray));
+
+//        anotherArray= new int[] {4,5,6,7,8};
+        modifyArray(myArray);
+
+        System.out.println("after modify myArray " + Arrays.toString(myArray));
+        System.out.println("after modify anotherArray " + Arrays.toString(anotherArray));
+        System.out.println("after modify thirdArray " + Arrays.toString(thirdArray));
+    }
+    private static void modifyArray(int[] array){
+
+        array[0]=2;
+        array=new int[]{1,2,3,4,5};
     }
 }
