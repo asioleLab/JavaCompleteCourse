9ee79cf5073c1b0752624d22ae1abe398aa9746d
escarsel <eloisa.scarsella@nttdata.com>
2022-11-23 18:47:56 +0100

    Static + final

---
+++ src/access_modifier/final_statement/Main.java
@@ -0,0 +1,31 @@
+package access_modifier.final_statement;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SomeClass one = new SomeClass("one");
+        SomeClass two = new SomeClass("two");
+        SomeClass three  = new SomeClass("three");
+
+        System.out.println(one.getInstanceNumber());
+        System.out.println(two.getInstanceNumber());
+        System.out.println(three.getInstanceNumber());
+
+        int pw= 674312;
+//        Password password = new Password(pw);
+        Password password = new ExtendedPassword(pw);
+        password.storePassword();
+
+        password.letMeIn(1);
+        password.letMeIn(523266);
+        password.letMeIn(9773);
+        password.letMeIn(0);
+        password.letMeIn(-1);
+        password.letMeIn(674312);
+
+        System.out.println("Main method called");
+        SIBTest test = new SIBTest();
+        test.someMethod();
+        System.out.println("Owner is " + SIBTest.owner);
+    }
+}
