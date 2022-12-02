23712d79dab59bf99541aaaae5ca3e68481a12ee
escarsel <eloisa.scarsella@nttdata.com>
2022-12-02 10:25:48 +0100

    Challenge collections

--- src/collections/adventure/challenge/Location_new.java
+++ src/collections/adventure/challenge/Location_new.java
@@ -3,12 +3,12 @@ package collections.adventure.challenge;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Location {
+public class Location_new {
     private final int locationID;
     private final String description;
     private final Map<String, Integer> exits;
 
-    public Location(int locationID, String description, Map<String,Integer> exits) {
+    public Location_new(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
         if(exits != null){
@@ -19,9 +19,7 @@ public class Location {
         this.exits.put("Q", 0);
     }
 
-//    public void addExit(String direction, int location) {
-//        exits.put(direction, location);
-//    }
+
     public int getLocationID() {
         return locationID;
     }
@@ -31,6 +29,6 @@ public class Location {
     }
 
     public Map<String, Integer> getExits() {
-        return new HashMap<String, Integer>(exits);
+        return exits;
     }
 }
