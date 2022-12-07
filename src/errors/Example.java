99bfcf9aefe8cf246b939e1089210662c821229e
escarsel <eloisa.scarsella@nttdata.com>
2022-12-07 14:58:23 +0100

    Errors

--- src/errors/Example.java
+++ src/errors/Example.java
@@ -6,25 +6,30 @@ import java.util.Scanner;
 public class Example {
 
     public static void main(String[] args) {
-        int result = divide();
-        System.out.println("Result: "+result);
+        try {
+            int result = divide();
+            System.out.println("Result: "+result);
+        }catch (ArithmeticException | NoSuchElementException e){
+            System.out.println(e.toString());
+            System.out.println("unable to perform division");
+        }
+
     }
 
     private static int divide(){
         int x ;
         int y ;
-        try{
+//        try{
             x = getInt();
             y = getInt();
-        }catch (NoSuchElementException e){
-            throw new ArithmeticException("no such element");
-        }
-        System.out.println("x is "+x+" , y is "+y);
-        try {
+            System.out.println("x is "+x+" , y is "+y);
             return x/y;
-        }catch (ArithmeticException e){
-            throw new ArithmeticException("attempt to divide by zero");
-        }
+//        }catch (NoSuchElementException e){
+//            throw new ArithmeticException("no such element");
+//        }catch (ArithmeticException e){
+//            throw new ArithmeticException("attempt to divide by zero");
+//        }
+
     }
 
     private static int getInt(){
